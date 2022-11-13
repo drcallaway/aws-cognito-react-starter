8dbb4b8fb444a3c585e21eb510ecb180bb04334f
dcallaway <dustin_callaway@intuit.com>
2022-11-12 17:39:40 -0700

    update IDs

--- src/auth/userPool.ts
+++ src/auth/userPool.ts
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-  UserPoolId: "us-east-1_yiQUTePFP",
-  ClientId: "342b61pt2vqsgmb7udhhdpj95c",
+  UserPoolId: "REPLACE_WITH_USER_POOL_ID",
+  ClientId: "REPLACE_WITH_APP_CLIENT_ID",
 };
 
 export default new CognitoUserPool(poolData);
